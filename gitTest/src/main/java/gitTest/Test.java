9202b6f36ce198dea95e3e98bf1dd6cd21138f7f
min <min@min-PC>
2018-06-25 14:19:56 +0900

    first

---
+++ gitTest/src/main/java/gitTest/Test.java
@@ -0,0 +1,15 @@
+package gitTest; 
+ /**
+ * <pre>
+ * 1. 개요        :
+ * 2. 처리내용    :
+ * </pre>
+ * @date          : 2018. 6. 22.
+ * @author        : min
+ * @history       :
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("TEST Git");
+	}
+}
